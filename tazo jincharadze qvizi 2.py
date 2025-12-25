da5597aabc15e2783775784eca075376ec964560
Tazo Jincharadze <tazojincharadze20@gmail.com>
2025-12-25 17:46:24 +0400

    Add files via upload
    MESIGI

---
+++ tazo jincharadze qvizi 2.py
@@ -0,0 +1,19 @@
+# personage = {
+#     "name": 'Tony Stark',
+#     "age": '53',
+#     "city": "New-York",
+#     "occupation": "billionaire industrialist, CEO of Stark Industries (formerly), genius inventor, and the superhero Iron Man",
+# }
+# print(personage)
+#
+# #3
+# personage["hobbie"] = "engineering, building suits, and saving the world"
+# print(personage)
+#
+#5
+def filter_dict(random_dict):
+    for key, value, in random_dict.items():
+        if value > 10:
+            print(key, value)
+
+filter_dict({"Python": 77, "Math": 9, })
