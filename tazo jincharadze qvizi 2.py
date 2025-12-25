14e4cd32342e4304262ecd896c4fd175fc11fc95
Giorgi Gogokhia <ggogokhia2@gmail.com>
2025-12-25 18:08:41 +0400

    davamate printi

--- tazo jincharadze qvizi 2.py
+++ tazo jincharadze qvizi 2.py
@@ -1,15 +1,15 @@
-# personage = {
-#     "name": 'Tony Stark',
-#     "age": '53',
-#     "city": "New-York",
-#     "occupation": "billionaire industrialist, CEO of Stark Industries (formerly), genius inventor, and the superhero Iron Man",
-# }
-# print(personage)
-#
-# #3
-# personage["hobbie"] = "engineering, building suits, and saving the world"
-# print(personage)
-#
+personage = {
+     "name": 'Tony Stark',
+     "age": '53',
+     "city": "New-York",
+     "occupation": "billionaire industrialist, CEO of Stark Industries (formerly), genius inventor, and the superhero Iron Man",
+ }
+print(personage)
+
+#3
+personage["hobbie"] = "engineering, building suits, and saving the world"
+print(personage)
+
 #5
 def filter_dict(random_dict):
     for key, value, in random_dict.items():
@@ -17,3 +17,5 @@ def filter_dict(random_dict):
             print(key, value)
 
 filter_dict({"Python": 77, "Math": 9, })
+
+print("pirveli cvlileba")
